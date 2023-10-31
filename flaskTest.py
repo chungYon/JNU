439acdeb28d6d493f88da6746afbcfade06cc6cf
chungYon <qwerty1076199@gmail.com>
2023-10-31 11:48:06 +0900

    auto install function added

--- flaskTest.py
+++ flaskTest.py
@@ -1,4 +1,16 @@
-from flask import Flask, render_template
+import sys
+import subprocess
+
+try:
+    # 없는 모듈 import시 에러 발생
+    from flask import Flask, render_template
+except:
+    # pip 모듈 업그레이드
+    subprocess.check_call([sys.executable,'-m', 'pip', 'install', '--upgrade', 'pip'])
+    # 에러 발생한 모듈 설치
+    subprocess.check_call([sys.executable,'-m', 'pip', 'install', '--upgrade', 'Flask'])
+    # 다시 import
+    from flask import Flask, render_template
 
 app = Flask(__name__)
 
